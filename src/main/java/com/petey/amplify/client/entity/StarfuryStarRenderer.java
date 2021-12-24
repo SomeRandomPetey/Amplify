54b94fe0f0efa72887667a6925a758487be07868
PeterTvedt <petertvedt01@gmail.com>
2021-12-24 18:19:26 -0500

    Trying to fix StarfuryStarEntity

--- src/main/java/com/petey/amplify/client/entity/StarfuryStarRenderer.java
+++ src/main/java/com/petey/amplify/client/entity/StarfuryStarRenderer.java
@@ -7,7 +7,6 @@ import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.model.ShulkerBulletModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
